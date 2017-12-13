56feddcbb5a3c3cc61174f079dc0a9ceb00471c0
Lo Whai Jer <neknek25251989@gmail.com>
2017-12-13 21:18:31 +0800

    [mod] Add if input is NaN.

--- js/original/js_comparisons.js
+++ js/original/js_comparisons.js
@@ -1,6 +1,10 @@
 function voteAge() {
   var age, voteable;
   age = document.getElementById('age').value;
-  voteable = (age < 18) ? "Too young":"Old enough"; // "?"回傳的是true ":"回傳的false
-  document.getElementById('vote').innerHTML = voteable + ' to vote.';
+  if (isNaN(age)) {
+    voteable = 'Input is not a number';
+  } else {
+    voteable = (age < 18) ? "Too young":"Old enough"; // "?"回傳的是true ":"回傳的false  
+  }
+  document.getElementById('vote').innerHTML = voteable;
 }
