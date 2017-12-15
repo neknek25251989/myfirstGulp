499e6f1b6a58d0100f1b8707ecf142c936c74646
Lo Whai Jer <neknek25251989@gmail.com>
2017-12-15 22:22:54 +0800

    [add] Exercise about JavaScritpt random

---
+++ js/original/js_random.js
@@ -0,0 +1,25 @@
+function myRandom0() {
+  document.getElementById('demo0').innerHTML = Math.random();
+}
+function myRandom1() {
+  document.getElementById('demo1').innerHTML = 
+  Math.floor(Math.random() * 10);
+}
+function myRandom2() {
+  document.getElementById('demo2').innerHTML = 
+  Math.floor(Math.random() * 11);
+}
+function myRandom3() {
+  document.getElementById('demo3').innerHTML = 
+  Math.floor(Math.random() * 100);
+}
+function myRandom4() {
+  document.getElementById('demo4').innerHTML =
+  Math.floor(Math.random() * 101);
+}
+function myRandom5() {
+  document.getElementById('demo5').innerHTML =
+  Math.floor(Math.random() * 10) + 1;
+  document.getElementById('demo55') .innerHTML =
+  Math.floor(Math.random() * 100) + 1;
+}
